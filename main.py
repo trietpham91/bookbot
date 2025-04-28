bf990874819685d0bcb2e816241f81cfad7060fb
trietpham <minhtriet9@gmail.com>
2025-04-27 23:50:32 -0400

    Test

--- main.py
+++ main.py
@@ -1,36 +1,26 @@
-from stats import (
-    get_num_words,
-    chars_dict_to_sorted_list,
-    get_chars_dict,
-)
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    chars_dict = get_chars_dict(text)
-    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
-    print_report(book_path, num_words, chars_sorted_list)
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+def sort_on(d):
+    return d["num"]
 
 
-def print_report(book_path, num_words, chars_sorted_list):
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for item in chars_sorted_list:
-        if not item["char"].isalpha():
-            continue
-        print(f"{item['char']}: {item['num']}")
-
-    print("============= END ===============")
-
-
-main()
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
